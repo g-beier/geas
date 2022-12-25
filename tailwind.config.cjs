9e541a5e77ea766bdc85ba0cb0c02d6f18f6700e
g-beier <gustavbeier@gmail.com>
2022-12-25 00:31:13 -0300

    Few contents added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,35 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
+// const colors = require('tailwindcss/colors');
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				primary: colors.blue,
-				secondary: colors.pink,
+				primary: {
+					50: '#E4EBF9',
+					100: '#C5D4F2',
+					200: '#A8BEEA',
+					300: '#8AA7E1',
+					400: '#6D91D8',
+					500: '#517BCF',
+					600: '#4769AD',
+					700: '#3D578C',
+					800: '#31446B',
+					900: '#24324C',
+				},
+				secondary: {
+					50: '#F7E9CC',
+					100: '#F1D8A7',
+					200: '#EAC781',
+					300: '#E3B65C',
+					400: '#DCA438',
+					500: '#D49313',
+					600: '#C58508',
+					700: '#B47700',
+					800: '#A16800',
+					900: '#8B5800',
+				},
 			},
 			fontFamily: {
 				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
